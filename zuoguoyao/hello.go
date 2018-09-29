c04a506a0013e13fc580177d634696c5d30e1985
zuoguoyao <zuoguoyao@gmail.com>
2018-09-29 20:33:46 +0800

    delete zuoguoyao

--- zuoguoyao/hello.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-func main() {
-	const LENGTH, WIDTH = 1000, 500
-	var area int
-	const a, b, c = 5, false, "str"
-	area = LENGTH * WIDTH
-	fmt.Println("面积：", area, a, b, c)
-}
